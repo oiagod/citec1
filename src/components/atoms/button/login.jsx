e3f9a7558f6f5aaed82923973c48881b254f5fa5
ogaidoa <iagodoarte2009@gmail.com>
2023-12-05 19:22:05 -0300

    Exceções tela de login nulo ou usuário inválido

--- src/components/atoms/button/login.jsx
+++ src/components/atoms/button/login.jsx
@@ -1,25 +1,43 @@
 import { Text, TouchableOpacity, StyleSheet } from "react-native";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-
+import Toast from "react-native-toast-message";
 const auth = getAuth();
 
 const LoginButton = ({ navigation, buttonText = 'Login', email, password }) => {
   const handleLogin = () => {
-    console.log("Email: ", email);
-    console.log("Password: ", password);
+    
+   if(!email || !password) {
+    Toast.show({
+      type: 'error',
+      text1: 'Há campos não preenchidos',
+      text2: 'Preencha todos os campos',
+      visibilityTime: 4000,
+    });
+
+    return; //impede a execução do restante do código
+   };
+
   signInWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
     const user = userCredential.user;
-
+    navigation.navigate("HomeScreen")
   })
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
-    console.log(errorMessage);
-    // ..
+
+    if (errorCode === "auth/wrong-passowrd" || errorCode === "auth/user-not-found" || errorCode === "auth/invalid-email") {
+      Toast.show({
+        type: 'error',
+        text1: 'Credenciais inválidas',
+        text2: 'Verifique seu e-mail e senha e tente novamente',
+        visibilityTime: 4000,
+      })
+    } else {
+      console.log(errorMessage);
+    }
   });
 
-    navigation.navigate("HomeScreen")
   };
   return (
     <TouchableOpacity style={styles.loginbutton} onPress={handleLogin}>
